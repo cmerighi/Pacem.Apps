18f432e01516e727be66cdedecd6f17605afaa83
Cristian Merighi <cristian@pacem.it>
2020-05-18 09:24:06 +0200

    Add project files.

---
+++ Pacem.Apps/Controllers/UpdateController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pacem.Apps.Controllers
+{
+    [ApiController, Route("api")]
+    public class UpdateController : ControllerBase
+    {
+        [HttpGet("check/{app}/{platform}/{version}")]
+        public Task<string> CheckAppVersion(string app, string platform, string version)
+        {
+
+        }
+    }
+}
