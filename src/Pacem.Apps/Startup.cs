ed77df2f82d0eb1b9d5ae22bd1054fbb32cc9555
Cristian Merighi <cristian@pacem.it>
2020-05-20 01:18:25 +0200

    Missing pipeline step.

--- src/Pacem.Apps/Startup.cs
+++ src/Pacem.Apps/Startup.cs
@@ -47,10 +47,13 @@ namespace Pacem.Apps
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UsePacemAcmeHttpChallenge();
             }
             else
             {
                 app.UseExceptionHandler("/Error");
+                app.UsePacemAcmeHttpChallenge();
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
                 app.UseHttpsRedirection();
